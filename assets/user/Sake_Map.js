050f8b1db3208c155f060ecb19a0317a1e7eb475
u-10bei <holy19690206@gmail.com>
2022-02-20 17:47:14 +0900

    draft2

---
+++ assets/user/Sake_Map.js
@@ -0,0 +1,17 @@
+var t_std = new L.tileLayer('http://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png', {
+    attribution: "<a href='https://maps.gsi.go.jp/development/ichiran.html' target='_blank'>国土地理院</a>"
+});
+var t_pale = new L.tileLayer('http://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png', {
+    attribution: "<a href='https://maps.gsi.go.jp/development/ichiran.html' target='_blank'>国土地理院</a>"
+});
+var Map_b = {
+    "地理院地図 標準": t_std,
+    "地理院地図 淡色": t_pale,
+};
+var map = L.map('map', {
+    center: [36.099887,138.75],
+    zoom: 5,
+    layers: [t_std]
+});
+L.control.scale({imperial:false}).addTo(map);
+L.control.layers(Map_b,null,null).addTo(map);
--- repository/_posts/2022-02-20-jp_SakeMap.md
+++ repository/_posts/2022-02-20-jp_SakeMap.md
@@ -5,11 +5,16 @@ subtitle:   "リポジトリ「Sake_Map」の紹介"
 date:   2022-02-20 01:00:00 +0900
 categories: repository
 background: '/img/posts/04.jpg'
+map: /assets/user/Sake_Map.js
 pagination: 
   enabled: true
   category: repository
   paginate_path: '/jekyll/:num/pagination-plugin/'
 ---
 ## リポジトリ「Sake_Map」の紹介
-- javascript、SQLiteを使い、北九州市オープンデータを折れ線グラフにしています。
-- 毎日２３時過ぎに、バッチファイルを使い、自動でデータを更新しています。
+- javascript、leafletを使い、私が今までに飲んだ主な日本酒を地図にプロットしています。
+
+---
+<div id="map" style="width:100%; height:500px;"></div>
+
+---
