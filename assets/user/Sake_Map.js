fd063f20238577784837b158bc2f16dd1402a90e
u-10bei <holy19690206@gmail.com>
2022-02-22 23:47:27 +0900

    Improved 2022/02/22

--- assets/user/Sake_Map.js
+++ assets/user/Sake_Map.js
@@ -15,3 +15,28 @@ var map = L.map('map', {
 });
 L.control.scale({imperial:false}).addTo(map);
 L.control.layers(Map_b,null,null).addTo(map);
+
+//$.getJSON("https://raw.githubusercontent.com/u-10bei/Sake_Map/main/Sake_Map.geojson?token=GHSAT0AAAAAABL7XIHJ5OY6JDXRFC4GYAUSYQUSSZQ", function (data) {
+//    L.geoJson(data).addTo(map);
+//});
+//var geojson_data = [{ "type": "FeatureCollection", "features": [{ "type": "Feature", "properties": {}, "geometry": { "type": "Point", "coordinates": [139.76712226867676, 35.68107370561057] } }, { "type": "Feature", "properties": {}, "geometry": { "type": "Point", "coordinates": [139.70030307769775, 35.69045025639499] } }, { "type": "Feature", "properties": {}, "geometry": { "type": "Point", "coordinates": [139.7512435913086, 35.707283453190406] } }] }
+//];
+
+//L.geoJson(geojson_data).addTo(map);
+
+const INPUT_ADDRESS = 'https://raw.githubusercontent.com/u-10bei/Sake_Map/main/Sake_Map.geojson'
+const NET_ERROR = '正常にリクエストを処理できませんでした。'
+const CATCH_ERROR = 'エラーが発生しました。'
+
+//const output_csv_el = document.querySelector('#output_csv');
+
+fetch(INPUT_ADDRESS)
+    .then(res => {
+        if(!res.ok) {
+            console.log(NET_ERROR);
+        }
+        return res.text();
+    })
+    .then(data => console.log(data))
+    .catch(error => {console.log(CATCH_ERROR,error);
+    })
