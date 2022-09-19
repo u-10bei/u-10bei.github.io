49b9e08b8d2d96bea9d2b70d870e4cd7dafad223
u-10bei <u10bei.github.io@gmail.com>
2022-09-19 20:40:09 +0900

    add newJS

---
+++ assets/user/Pedigree_JPG1horses2.js
@@ -0,0 +1,84 @@
+const LINKURL = 'https://labo.u10bei.net/PHP/StratifyLink.php'
+
+d3.json(LINKURL)
+.then(function(LINKALL){
+    const data_h = d3.stratify()
+    .id(function(d) { return d.from; })
+    .parentId(function(d) { return d.to; })
+    (LINKALL);
+
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+
+    const tree = (data) => {
+        const root = d3.hierarchy(data);
+        root.dx = 20;
+        root.dy = width / (root.height + 1);
+        return d3.tree().nodeSize([root.dx, root.dy])(root);
+    };
+
+    const root = tree(data_h);
+
+    const zoom = d3.zoom().scaleExtent([0.5, 10]).on("zoom", zoomed);
+
+    const svg = d3.select("#mychart")
+    .append("svg")
+        .attr("viewBox", [0, 0, width, height])
+        .attr("cursor", "move");
+
+    svg.call(zoom);
+
+    const g = svg
+        .append("g")
+        .attr("font-family", "sans-serif")
+        .attr("font-size", 12)
+        .attr("transform", `translate(${root.dx * 2}, ${root.dy})`);
+
+    const link = g
+        .append("g")
+        .attr("fill", "none")
+        .attr("stroke", "#555")
+        .attr("stroke-opacity", 0.4)
+        .attr("stroke-width", 1.5)
+        .selectAll("path")
+        .data(root.links())
+        .join("path")
+        .attr(
+            "d",
+            d3
+            .linkHorizontal()
+            .x((d) => d.y)
+            .y((d) => d.x)
+        );
+
+    const node = g
+        .append("g")
+        .attr("stroke-linejoin", "round")
+        .attr("stroke-width", 3)
+        .selectAll("g")
+        .data(root.descendants())
+        .join("g")
+        .attr("transform", (d) => `translate(${d.y}, ${d.x})`)
+    
+        node
+        .append("text")
+        .attr("x", d => d.children ? -5 : 5)
+        .attr("dy", "0.31em")
+        .attr("text-anchor", d => d.children ? "end" : "start")
+        .attr("font-size", d => d.children ? 5 : 12)
+        .text(d => d.data.id)
+        .clone(true)
+        .lower()
+        .attr("stroke", "white");
+    
+        node
+        .append("circle")
+        .attr("fill", "#999")
+        .attr("r", 3);
+   
+    function zoomed(event) {
+        const { transform } = event;
+        g.attr("transform", transform);
+        g.attr("stroke-width", 1 / transform.k);
+    }
+});
---
+++ repository/_posts/2022-09-17-jp_Pedigree_JPG1horses2.md
@@ -0,0 +1,28 @@
+---
+layout: post
+title:  "Pedigree_JPG1horsesについて２"
+subtitle:   "リポジトリ「Pedigree_JPG1horses」の紹介２"
+date:   2022-09-08 00:00:00 +0900
+categories: repository
+background: '/img/posts/06.jpg'
+hierarchy: '/assets/user/Pedigree_JPG1horses2.js'
+pagination: 
+  enabled: true
+  category: repository
+  paginate_path: '/jekyll/:num/pagination-plugin/'
+---
+
+## リポジトリ「Pedigree_JPG1horses」の紹介
+- 前回、vis.jsでグラフを作成しましたが、  
+- データ数の増加とともに描画に時間がかかるようになったので、  
+今回は、mariaDB、PHP、D3.js、javascriptを使い、  
+日本のＧ１（１９８３年までの旧八大競走、ジャパンカップを含む）  
+勝利馬の血統を、階層図にしています。
+
+---
+
+<div id="mychart"></div>
+
+---
+
+- プログラムの中身を知りたい方は[**こちら**](https://github.com/u-10bei/Pedigree_JPG1horses)
